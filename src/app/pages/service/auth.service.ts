25c2d9c750b21d95ce22d22d79eaac534889b8a2
Harnas <rockypiotr72@gmail.com>
2025-02-12 19:34:29 +0100

    Add base login page

---
+++ src/app/pages/service/auth.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { LoginRequest, LoginResponse } from '../../@shared/api/auth';
+
+interface ILoginData {
+  username: string;
+  password: string;
+}
+
+interface Token {
+  token: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly apiUrl = environment.apiUrl;
+  private readonly http = inject(HttpClient);
+
+  login(loginData: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.apiUrl}/auth/login`, loginData);
+  }
+
+  setCredentials(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getAuthToken() {
+    return localStorage.getItem('token');
+  }
+}
