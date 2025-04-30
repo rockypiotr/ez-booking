67793fd02fa2ed75155b3ee5465a1d47d6765100
Harnas <rockypiotr72@gmail.com>
2025-04-30 16:40:02 +0200

    Update border radius in theme

--- Frontend/src/app/theme-preset.ts
+++ Frontend/src/app/theme-preset.ts
@@ -7,9 +7,9 @@ export const themePreset = definePreset(Aura, {
       none: '0',
       xs: '2px',
       sm: '4px',
-      md: '6px',
-      lg: '8px',
-      xl: '12px',
+      md: '8px',
+      lg: '16px',
+      xl: '24px',
     },
     emerald: {
       50: '#ecfdf5',
