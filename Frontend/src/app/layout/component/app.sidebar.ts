756a4001cbc5e3e062531e34119a0a8a532e7e61
rockypiotr <rockypiotr72@gmail.com>
2025-05-31 17:29:32 +0200

    changed gradient orientation in sidebar

--- Frontend/src/app/layout/component/app.sidebar.ts
+++ Frontend/src/app/layout/component/app.sidebar.ts
@@ -1,54 +1,53 @@
-import {Component, ElementRef, inject} from '@angular/core';
-import {Avatar} from 'primeng/avatar';
-import {Button} from 'primeng/button';
-import {AppMenu} from './app.menu';
-import {AuthService} from "../../pages/service/auth.service";
+import { Component, inject } from '@angular/core';
+import { Avatar } from 'primeng/avatar';
+import { Button } from 'primeng/button';
+import { AuthService } from '../../pages/service/auth.service';
+import { AppMenu } from './app.menu';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [AppMenu, Button, Avatar],
-  template: `
-    <div class="layout-sidebar shadow-lg">
-      <div class="pt-3 pb-8">
-        <i class=""></i>
-        <span
-          class="text-5xl bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 font-extrabold bg-clip-text text-transparent"
+  template: ` <div class="layout-sidebar shadow-lg">
+    <div class="pt-3 pb-8">
+      <i class=""></i>
+      <span
+        class="text-5xl bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 font-extrabold bg-clip-text text-transparent"
         >EZ-Booking</span
-        >
-      </div>
-      <app-menu></app-menu>
-      <div class="mt-auto rounded-lg p-4 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
-        <div class="flex flex-col items-center gap-5">
+      >
+    </div>
+    <app-menu></app-menu>
+    <div class="mt-auto rounded-lg p-4 bg-gradient-to-br from-purple-500 via-pink-500 to-red-500">
+      <div class="flex flex-col items-center gap-5">
         <span class="text-white text-center text-lg font-bold"
-        >Upgrade to PRO to get access all Features!</span
+          >Upgrade to PRO to get access all Features!</span
         >
-          <p-button severity="secondary" label="Get pro now!" raised rounded></p-button>
-        </div>
+        <p-button severity="secondary" label="Get pro now!" raised rounded></p-button>
       </div>
-      <div class="flex items-center pb-3 py-9 gap-2">
-        <div>
-          <p-avatar
-            image="https://primefaces.org/cdn/primeng/images/demo/avatar/asiyajavayant.png"
-            size="large"
-            shape="circle"
-          ></p-avatar>
-        </div>
-        <div class="flex flex-col">
-          <strong class="text-xl">Łukasz H</strong>
-          <span>Administrator</span>
-        </div>
-        <p-button
-          type="button"
-          icon="pi pi-sign-out"
-          ariaLabel="sign out"
-          (onClick)="logout()"
-          text
-          class="layout-topbar-action ml-auto"
-          rounded
-        />
+    </div>
+    <div class="flex items-center pb-3 py-9 gap-2">
+      <div>
+        <p-avatar
+          image="https://primefaces.org/cdn/primeng/images/demo/avatar/asiyajavayant.png"
+          size="large"
+          shape="circle"
+        ></p-avatar>
       </div>
-    </div>`,
+      <div class="flex flex-col">
+        <strong class="text-xl">Łukasz H</strong>
+        <span>Administrator</span>
+      </div>
+      <p-button
+        type="button"
+        icon="pi pi-sign-out"
+        ariaLabel="sign out"
+        (onClick)="logout()"
+        text
+        class="layout-topbar-action ml-auto"
+        rounded
+      />
+    </div>
+  </div>`,
 })
 export class AppSidebar {
   private readonly authService = inject(AuthService);
